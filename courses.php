901ad761116c595a95b2699697939c71ba8016ac
patel371998 <40248297+patel371998@users.noreply.github.com>
2019-06-21 16:18:49 +0530

    2

--- courses.php
+++ courses.php
@@ -4,13 +4,14 @@ include("dbconnect.php");
 
 <?php include 'header.php';?>
 			<!-- start banner Area -->
-			<section class="banner-area relative about-banner" id="home" style="background:url('img/bnn1.jpg');>	
+			<section class="banner-area relative about-banner" id="home" style="background:url('img/bnn1.jpg')";>	
+				<div class="overlay overlay-bg"></dinn1
 				<div class="overlay overlay-bg"></div>
 				<div class="container">				
 					<div class="row d-flex align-items-center justify-content-center">
 						<div class="about-content col-lg-12">
 							<h1 class="text-white">
-								 COURSES OFFERED		
+								 Courses Offered		
 							</h1>	
 							
 						</div>	
